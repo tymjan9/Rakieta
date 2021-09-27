a5b3e963adc4cc2eaa74d77a2df9619c1f8a8a4a
Stachu1 <77758413+Stachu1@users.noreply.github.com>
2021-09-27 14:42:08 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -7,15 +7,19 @@ class Rocket:
 
         self.positon = self.settings.rocket_start_postition
         self.velocity = self.settings.rocket_start_velocity
-        self.thrust = 10000
+        self.thrust = 35000000
 
 
     def symulate_next_step(self):
 
+
         self.velocity[0] = self.velocity[0] + 0 * self.settings.delta_t
         self.velocity[1] = self.velocity[1] + (self.settings.gravitational_acceleration + self.thrust / self.settings.rocket_mass) * self.settings.delta_t
 
-        print(self.velocity)
+        self.positon[0] = self.positon[0] + self.velocity[0] * self.settings.delta_t + 0 * self.settings.delta_t**2 / 2
+        self.positon[1] = self.positon[1] + self.velocity[1] * self.settings.delta_t + (self.settings.gravitational_acceleration + self.thrust / self.settings.rocket_mass) * self.settings.delta_t ** 2 / 2
+
+        # print(self.velocity, "   ", self.positon)
 
 
 
