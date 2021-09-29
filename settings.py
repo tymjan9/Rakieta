515b2692316821ee7af25d56592ee8db7ec8be5c
Stachu1 <stach.basiuk@gmail.com>
2021-09-29 10:20:50 +0200

    coś się popsuło

--- settings.py
+++ settings.py
@@ -4,8 +4,8 @@ import os
 
 class Settings:
     def __init__(self):
-
-        self.rocket_mass = 1725000
+        self.fuel = 2550000/2
+        self.rocket_mass = 450000 + self.fuel
         self.rocket_max_thrust = 35000000
         self.rocket_max_torque = 100000
         self.rocket_start_postition = [0,0]
